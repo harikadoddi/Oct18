3e4e609993b6fd2f95f56e25530ef75ae9ef2b5c
harika <harikad2000@gmail.com>
2024-11-08 20:27:40 +0530

    Basic scala

--- src/main/scala/hello.scala
+++ src/main/scala/hello.scala
@@ -1,14 +1,20 @@
 import org.apache.spark.SparkContext
 
-object hello {
-  def main(args: Array[String]): Unit = {
-    val sc= new SparkContext("local[1]","harika")
-    val input = sc.textFile("C:/Users/Admin/OneDrive/Desktop/data.txt")
-    val rdd1 = input.flatMap(x => x.split(" "))
-    val rdd2 = rdd1.map(x => (x,1))
-    val rdd3 = rdd2.reduceByKey((x,y) => x+y)
-    val rdd4 = rdd3.sortBy(x => x._2,true)
-    rdd4.take(2).foreach(println)
+import java.io.{File, PrintWriter}
 
+object hello{
+  def main(args: Array[String]): Unit = {
+      val sc = new SparkContext("local[*]","harika")
+      val data = sc.textFile("C:/Users/Admin/OneDrive/Desktop/info.txt")
+      val file = new File("C:/Users/Admin/OneDrive/Desktop/modified.txt")
+      val data1 = data.map(x => x.toInt)
+
+      val printwriter = new PrintWriter(file)
+      val result = data1.map{x => if(x%2==0){ x+"-"+x.toString.length+"-even"}
+        else{ (x+"-"+x.toString.length+"-odd")}}.collect()
+
+      result.foreach(line => printwriter.write(line+ "\n"))
+
+      printwriter.close()
   }
 }
